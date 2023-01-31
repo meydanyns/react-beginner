ad4e777d13d1f2fe836b336548bc2435c6452105
meydanyns <123954609+meydanyns@users.noreply.github.com>
2023-01-31 14:46:17 +0300

    yeniden gönderim

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import logo from './logo.svg'
 function App() {
   return (
     <div className="App">
-     <h3>{Process.env.NODE_ENV}yunus</h3>
+     <h3>{process.env.NODE_ENV}</h3>
+
+<p>
+{process.env.REACT_APP_API_URL}
+
+</p>
+
+
+     {process.env.NODE_ENV == 'production' && (
+
+      <div>
+        <img src="/logo192.png"  alt=""/>
+        <img src="/logo192.png"  alt=""/>
+      <div/>
+     )
+
+
+     }
      
      <img  src="/logo192.png" alt=""/>
      <img src={logo}  alt="" />
     </div>
-  );
-}
+  )
 
+    }
 export default App;
 
